78c261fef1faf7e81cbd771a4e7f943045d161d1
Amelie-Sausseau <the_world_is_mine17@hotmail.fr>
2021-03-10 20:22:57 +0100

    Contact form style modifications

--- portfolio/src/Form/ContactType.php
+++ portfolio/src/Form/ContactType.php
@@ -8,18 +8,28 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ContactType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', TextType::class)
+            ->add('name', TextType::class,[
+                'constraints' => [
+                    new NotBlank()
+                ]
+            ])
 
-            ->add('email', EmailType::class)
+            ->add('email', EmailType::class,[
+                'constraints' => [
+                    new NotBlank()
+                ]
+            ])
 
             ->add('message', TextareaType::class, [
-                'attr' => ['rows' => 5]
+                'attr' => ['rows' => 5],
+                'constraints' => [new NotBlank()]
             ])
         ;
     }
