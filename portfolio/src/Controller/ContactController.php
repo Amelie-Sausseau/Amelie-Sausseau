a6fccb75924ff39336be619fb86e9bde3153e6df
Amelie-Sausseau <the_world_is_mine17@hotmail.fr>
2021-03-10 20:14:19 +0100

    Contact form

---
+++ portfolio/src/Controller/ContactController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function index(Request $request, MailerInterface $mailer): Response
+    {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $contactFormData = $form->getData();
+
+            $message = (new Email())
+                ->from($contactFormData['email'])
+                ->to('amelie.sausseau@gmail.com')
+                ->subject('Nouveau message depuis le portfolio')
+                ->text(
+                    'Envoyé par : '.$contactFormData['name'] . '(' .$contactFormData['email'].')'.\PHP_EOL.
+                    'Message :' . $contactFormData['message'],
+                    'text/plain'
+                );
+
+            $mailer->send($message);
+
+            $this->addFlash('success', 'Votre message a bien été envoyé');
+
+            return $this->redirectToRoute('contact');
+        }
+            return $this->render('contact/index.html.twig', [
+            'controller_name' => 'ContactController',
+            'form' => $form->createView(),
+        ]);
+        
+    }
+}
