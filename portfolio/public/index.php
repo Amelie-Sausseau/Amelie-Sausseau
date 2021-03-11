fdb32b8f20e85fdaa96f69939b4440d44fbad1f5
Amelie-Sausseau <the_world_is_mine17@hotmail.fr>
2021-03-11 18:03:48 +0100

    index.php modifications

--- portfolio/public/index.php
+++ portfolio/public/index.php
@@ -9,6 +9,13 @@ require dirname(__DIR__).'/vendor/autoload.php';
 
 (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 
+$trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false;
+$trustedProxies = $trustedProxies ? explode(',', $trustedProxies) : [];
+if($_SERVER['APP_ENV'] == 'prod') $trustedProxies[] = $_SERVER['REMOTE_ADDR'];
+if($trustedProxies) {
+    Request::setTrustedProxies($trustedProxies, Request::HEADER_X_FORWARDED_AWS_ELB);
+}
+
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
 
