be9ad13531660a3d7cca75449d3fe766700c7f9a
Amelie-Sausseau <the_world_is_mine17@hotmail.fr>
2021-03-11 16:33:07 +0100

    Folder modifications

--- portfolio/src/Controller/ProjectsController.php
+++ portfolio/src/Controller/ProjectsController.php
@@ -9,11 +9,31 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProjectsController extends AbstractController
 {
     /**
+     * @Route("/projets", name="projects")
+     */
+    public function projects(): Response
+    {
+        return $this->render('projects/index.html.twig', [
+            'controller_name' => 'ProjectsController',
+        ]);
+    } 
+
+    /**
      * @Route("/kroon", name="kroon")
      */
     public function kroon(): Response
     {
         return $this->render('projects/kroon.html.twig', [
+            'controller_name' => 'ProjectsController',
+        ]);
+    }
+
+    /**
+     * @Route("/portfolio", name="portfolio")
+     */
+    public function portfolio(): Response
+    {
+        return $this->render('projects/portfolio.html.twig', [
             'controller_name' => 'ProjectsController',
         ]);
     }
